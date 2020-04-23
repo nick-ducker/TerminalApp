4c612fcb47bd3e67da2e05cfa01cc91d3d69b6ad
62416837 <nick_ducker@live.com>
2020-04-23 13:31:14 +0930

    Creates functions module and adds methods to pass tests

--- src/modules/functions.rb
+++ src/modules/functions.rb
@@ -1,6 +1,6 @@
 module FunctionsModule
 
-    COLOURS = [
+    @@colours = [
         :green,
         :light_red,
         :red,
@@ -14,5 +14,38 @@ module FunctionsModule
         :cyan,
         :light_cyan,
     ]
+    
+    #reader method for @@colours
+    def colours
+        return @@colours
+    end
+
+    #changes colour to passed key
+    def colour_changer(key)
+        @@colours = [key]
+    end
+
+    #returns a string of score and difficulty
+    def score(score,difficulty)
+        return "Current Score: #{score}  |  Difficulty: #{difficulty}"
+    end
+
+    #selects a random element from an array
+    def selector(array)
+        return array.shuffle.first
+    end
+
+    #deletes a passed variable from an array if found
+    def deleter(variable,array)
+        array.delete(variable)
+        return array
+    end
+
+    #checks input against a prompt and returns either true, false or no answer
+    def checker(input,prompt)
+        input == "no answer" ? (return input) : nil
+        match = prompt.split.find{|x| x[0] == "["}.delete("[]")
+        input.casecmp(match) == 0 ? (return "true") : (return "false") 
+    end
 
 end
