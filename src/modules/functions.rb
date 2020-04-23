3f3cff015c4079258a55af994bc613956fc3dcda
62416837 <nick_ducker@live.com>
2020-04-23 12:51:36 +0930

    Creates functions module and adds readable colours constant

---
+++ src/modules/functions.rb
@@ -0,0 +1,18 @@
+module FunctionsModule
+
+    COLOURS = [
+        :green,
+        :light_red,
+        :red,
+        :light_green,
+        :yellow,
+        :light_yellow,
+        :blue,
+        :light_blue,
+        :magenta,
+        :light_magenta,
+        :cyan,
+        :light_cyan,
+    ]
+
+end
